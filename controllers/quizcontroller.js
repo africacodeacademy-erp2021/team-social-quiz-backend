d02a991614b5569b88537528a6466eb70fcb9d34
Gakegakwe <87799316+Gakegakwe@users.noreply.github.com>
2021-09-22 14:33:41 +0200

    Built-in Validators added to Schema

--- controllers/quizcontroller.js
+++ controllers/quizcontroller.js
@@ -1,7 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/test');
+  await mongoose.connect("mongodb://localhost:27017/test");
 }
+
+const quizSchema = new mongoose.Schema({
+  published_state:{
+  type: String,  
+  required:[true,'Published State required'] 
+  }, 
+  popularity:{
+     type:String,
+     required: [true,'Popularity required'] 
+  },
+  questions_id: {
+    type: String,
+    unique: true,
+    required: [true,'Questions ID required'] 
+  },
+});
+
+const Quiz = mongoose.model('Quiz', quizSchema);
+
+const quiz=new Quiz();
+
+Quiz.path("questions_id").validate(function (value) {
+  this.model("Quiz").count({ questions_id: value }, function (error, count) {
+    if (error) {
+       console.log(error);  
+      return error;
+     
+    }
+    else{
+        Quiz.save();
+    }
+  });
+});
+
