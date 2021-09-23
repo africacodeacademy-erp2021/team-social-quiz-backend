623cd3c809dc41979a95ea87e53c99c483f21ced
Gakegakwe <87799316+Gakegakwe@users.noreply.github.com>
2021-09-23 10:06:36 +0200

    middleware validation

--- middleware/quizmiddleware.js
+++ middleware/quizmiddleware.js
@@ -24,7 +24,13 @@ const quizSchema = new mongoose.Schema({
   const Quiz = mongoose.model('Quiz', quizSchema);
   const quiz=new Quiz();
   
-  quiz.pre('save', function (req, res) {
+  quiz.pre('save',[
+    check('published_state', 'published state should be a boolean').not().isEmpty().isBoolean(),
+    check('popularity', 'popularity should be a number').not().isEmpty().isInt(),
+    check('category_id', 'category id should be a string').not().isEmpty().isString(),
+    check('questions_id', 'questions id should be a string').not().isEmpty().isString(),
+  ],
+   function (req, res) {
     const errors = validationResult(req);
     console.log(req.body);
 
