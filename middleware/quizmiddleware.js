3d0eb71046803e13865cb802e81264639c4972a3
Gakegakwe <87799316+Gakegakwe@users.noreply.github.com>
2021-09-25 08:45:47 +0200

    middleware validation update

--- middleware/quizmiddleware.js
+++ middleware/quizmiddleware.js
@@ -1,35 +1,32 @@
-exports.validateQuiz = async (req, res) => {
-   try {
-      if(!req.body.title.isString()){
-         console.error("Value should be only characters");
-      }
-     else if(!req.body.description.isString()){
-         console.error("Value should be only characters");
-      }
-      else if(!Array.isArray(req.body.questions)){
-         console.error("Value should be an array");
-      }
-      else if(!req.body.category_id.isString()){
-         console.error("Value should be only characters");
-      }
-      else if(!Array.isArray(req.body.games)){
-         console.error("Value be an array");
-      }
-      else if(!req.body.is_published.isBoolean()){
-         console.error("Value should be boolean");
-      }
-      else if(!req.body.popularity.isInteger()){
-         console.error("Value should be number");
-      }
-      else if(!req.body.total_score.isInteger()){
-         console.error("Value should be a number");
-      }
-      else {
-       res.send({});
-       next();
-     }
-
-   } catch (error) {
+exports.validateQuiz = async (req, res, next) => {
+  try {
+    if (req.body.title.isString()) {
+      next();
+    }
+    if (req.body.description.isString()) {
+      next();
+    }
+    if (Array.isArray(req.body.questions)) {
+      next();
+    }
+    if (req.body.category_id.isString()) {
+      next();
+    }
+    if (Array.isArray(req.body.games)) {
+      next();
+    }
+    if (req.body.is_published.isBoolean()) {
+      next();
+    }
+    if (req.body.popularity.isInteger()) {
+      next();
+    }
+    if (req.body.total_score.isInteger()) {
+      next();
+    } else {
       return res.sendStatus(500);
     }
-   }
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+};
