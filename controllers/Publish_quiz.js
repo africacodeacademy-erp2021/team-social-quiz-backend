18fcc4d2e99b3f1e4e0f2587ffa390e6a87716ff
bulaneNayo <bulane.naeo@bothouniversity.com>
2021-09-26 19:34:57 +0200

    set published to true and return updated quiz

---
+++ controllers/Publish_quiz.js
@@ -0,0 +1,26 @@
+const { Quiz } = require("../models/Quiz");
+const express = require("express");
+const router = express.Router();
+
+exports.Quiz = async (req, res) => {
+  /**
+   * find Quiz, set its published status to true and return the updated quiz.
+   */
+
+  router.get("/Quiz", async (req, res) => {
+    try {
+      const QuizToPublish = await quizzes
+        .findByIdAndUpdate(_id, { $set: { is_published: true } })
+        .exec();
+
+      QuizToPublish.title = tittle;
+
+      await QuizToPublish.save();
+
+      return res.status(200).json(Quiz);
+      
+    } catch (error) {
+      return res.status(500).json({ error: error });
+    }
+  });
+};
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express();
-const Quiz_input = require("../controllers/Quiz_input");
-const Quiz_Validation = require("../middleware/Quiz_Validation");
-
-router.get("/", Quiz_input.index);
-router.post("/Quiz", validation.Quiz, Quiz_input.Quiz)
