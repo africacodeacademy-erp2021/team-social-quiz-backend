eb586a8e911572a89482261f7da378612b845e7b
bulaneNayo <bulane.naeo@bothouniversity.com>
2021-09-26 21:25:01 +0200

    remove quiz from public view

--- controllers/Publish_quiz.js
+++ controllers/Publish_quiz.js
@@ -7,7 +7,7 @@ exports.Quiz = async () => {
    * find Quiz, set its published status to true and return the updated quiz.
    */
 
-  router.get("/models/Quiz", async (req, res) => {
+  router.get("/Quiz", async (req, res) => {
     try {
       const { title } = req.body;
       const QuizToPublish = await quizzes
@@ -19,6 +19,29 @@ exports.Quiz = async () => {
       await QuizToPublish.save();
 
       return res.status(200).json(QuizToPublish);
+    } catch (error) {
+      return res.status(500).json({ error: error });
+    }
+  });
+
+  /**
+   * find and remove Quiz from public view
+   */
+
+  router.delete("/Quiz/:id", (req, res) => {
+    try {
+      if (is_published === true) {
+        const deleteQuestion = await quizzes
+          .findOneAndRemove({ _id })
+          .exec()
+          .then((Quiz) => res.send(`${Quiz.title}removed from public view`));
+
+        if ((deleteQuestion.deleteCount = 0)) {
+          return res.status(404).json();
+        } else {
+          return res.status(204).json();
+        }
+      }
     } catch (error) {
       return res.status(500).json({ error: error });
     }
