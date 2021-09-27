213cce3d82da778c5a9e105087b6f1f17b05511c
Gakegakwe <87799316+Gakegakwe@users.noreply.github.com>
2021-09-27 12:40:21 +0200

    get all games played by user controller

--- controllers/user.js
+++ controllers/user.js
@@ -17,3 +17,16 @@ exports.getAllUsers = async (req, res) =>{
     }
     
 }
+exports.getAllGames = async (req, res) => {
+    try {
+      let player = await Quiz.findOne(req.body.email);
+  
+      if (player.game_history.length > 0) {
+        return res.send(player.game_history);
+      } else {
+        return res.status(204);
+      }
+    } catch (error) {
+      return res.sendStatus(500);
+    }
+  };
