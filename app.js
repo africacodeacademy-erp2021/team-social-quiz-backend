8e881add7c75a545ee4e84ca1ff46d1a0d25b45f
Gakegakwe <87799316+Gakegakwe@users.noreply.github.com>
2021-09-28 15:40:38 +0200

    routing system

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 /**
  * Module dependencies.
  */
- const express = require("express");
- const chalk = require("chalk");
- const dotenv = require("dotenv");
- const mongoose = require("mongoose");
- const mongoSanitize = require('express-mongo-sanitize');
+const express = require("express");
+const chalk = require("chalk");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const mongoSanitize = require("express-mongo-sanitize");
 
 /**
  * Create Express server.
  */
 const app = express();
 
- /**
+/**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
 dotenv.config({
-    path: ".env"
-  });
+  path: ".env",
+});
 
 // TODO: Data Sanitization against XSS
 
 //sanitize requests against special chars, some precaution against NoSQL Injection Attacks
-app.use(mongoSanitize())
+app.use(mongoSanitize());
 
- /**
+/**
  * Connect to MongoDB.
  */
 mongoose.set("useFindAndModify", false);
@@ -32,10 +32,9 @@ mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
 mongoose.set("useNewUrlParser", true);
 
-
 mongoose.connect(process.env.MONGO_URI);
 
-mongoose.connection.on("error", err => {
+mongoose.connection.on("error", (err) => {
   console.error(err);
   console.log(
     "%s MongoDB connection error. Please make sure MongoDB is running.",
@@ -47,17 +46,32 @@ mongoose.connection.on("error", err => {
 /**
  * Controllers.
  */
- const userController = require("./controllers/user");
- const quizController = require("./controllers/quiz");
-
- 
+const userController = require("./controllers/user");
+const quizController = require("./controllers/quiz");
 /**
  * User Routes
- * 
+ *
  * TODO: Protect user routes
  */
- 
- app.post("/createQuiz",quizController.createQuiz);
+app.get("/users", userController.getAllUsers);
+app.get("/getAllQuiz", (req, res) => {
+  quizController.getAllQuiz;
+});
+app.get("/getOneQuiz", (req, res) => {
+  quizController.getOneQuiz;
+});
+app.get("/getAllQs", (req, res) => {
+  quizController.getAllQs;
+});
+app.get("/getOneQ", (req, res) => {
+  quizController.getOneQ;
+});
+app.post("/updateQuiz", (req, res) => {
+  quizController.updateQuiz;
+});
+app.post("/createQuiz", (req, res) => {
+  quizController.createQuiz;
+});
 
 /**
  * Express configuration.
@@ -65,13 +79,12 @@ mongoose.connection.on("error", err => {
 app.set("host", "127.0.0.1");
 app.set("port", process.env.PORT);
 
-
 app.listen(app.get("port"), () => {
-    console.log(
-        "%s App is running at http://localhost:%d in %s mode",
-        chalk.green("✓"),
-        app.get("port")
-    );
+  console.log(
+    "%s App is running at http://localhost:%d in %s mode",
+    chalk.green("✓"),
+    app.get("port")
+  );
 });
 
 module.exports = app;
