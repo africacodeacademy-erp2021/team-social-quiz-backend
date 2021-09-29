dcb04d7dac32777c889a27b4b0331526c37aadc7
bulaneNayo <bulane.naeo@bothouniversity.com>
2021-09-29 09:23:46 +0200

    elevate user role

--- routes/AllUsers.js
+++ routes/AllUsers.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const { Role } = require("../utils/Constants");
+const {getAllUsers} = require("../utils/Constants");
 
-exports.getAllUsers = async(req, res)=>{
+exports.getAllUsers = async()=>{
 
     try{
-        router.get('/users', async (req, res) => {
+        router.get('/User', async (req, res) => {
             try {
                 const userList = await User.find()
                 return res.status(200).json(userList)
@@ -16,5 +18,27 @@ exports.getAllUsers = async(req, res)=>{
         return res.sendStatus(500)
     }
 }
+
+
+exports.ChangeUserRole = async()=>{
+
+    
+        router.get('/User', async (req, res) => {
+            try {
+               if(Role.PLAYER === player) {
+                const role = users.findByIdAndUpdate(_id, { $set: { role: Role.ADMIN } })
+                .exec();
+                 return res.status(200).json(role);
+               }
+                
+            } catch (error) {
+                console.log(error);
+                return res.status(500).json({"error":error})
+            }
+        })
+    }
+
+
+
 
 
