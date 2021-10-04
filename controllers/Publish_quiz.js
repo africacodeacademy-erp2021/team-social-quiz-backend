e8c26239a5207cf56a9816043c134d5b3986d297
bulaneNayo <bulane.naeo@bothouniversity.com>
2021-10-04 14:13:45 +0200

    published quiz

--- controllers/Publish_quiz.js
+++ controllers/Publish_quiz.js
@@ -1,48 +1,38 @@
 const { Quiz } = require("../models/Quiz");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URI);
 const express = require("express");
 const router = express.Router();
 
-exports.Quiz = async () => {
-  /**
-   * find Quiz, set its published status to true and return the updated quiz.
-   */
+/**
+ * find Quiz by id, set its published status to true and return the updated quiz.
+ */
 
-  router.get("/Quiz", async (req, res) => {
+exports.QuizPublish = async () => {
+  router.put("/Quiz/:_id", async (req, res) => {
     try {
-      const { title } = req.body;
-      const QuizToPublish = await quizzes
-        .findByIdAndUpdate(_id, { $set: { is_published: true } })
-        .exec();
+      if (is_published === false) {
+        const { title,description,questions,category_id,games,is_published,popularity,total_score } = req.body;
+        const QuizToPublish = await quizzes
+          .findByIdAndUpdate(_id, { $set: { is_published: true } })
+          .exec();
 
-      QuizToPublish.title = title;
+        QuizToPublish.title = title;
+        QuizToPublish.description = description;
+        QuizToPublish.questions = questions;
+        QuizToPublish.category_id = category_id;
+        QuizToPublish.games = games;
+        QuizToPublish.popularity = popularity;
+        QuizToPublish.total_score = total_score;
 
-      await QuizToPublish.save();
+        await QuizToPublish.save();
 
-      return res.status(200).json(QuizToPublish);
-    } catch (error) {
-      return res.status(500).json({ error: error });
-    }
-  });
-
-  /**
-   * find and remove Quiz from public view
-   */
-
-  router.delete("/Quiz/:id", (req, res) => {
-    try {
-      if (is_published === true) {
-        const deleteQuestion = await quizzes
-          .findOneAndRemove({ _id })
-          .exec()
-          .then((Quiz) => res.send(`${Quiz.title}removed from public view`));
-
-        if ((deleteQuestion.deleteCount = 0)) {
-          return res.status(404).json();
-        } else {
-          return res.status(204).json();
-        }
+        return res.status(200).json(QuizToPublish);
+      } else {
+        return res.status(404).json();
       }
     } catch (error) {
+      console.log(error);
       return res.status(500).json({ error: error });
     }
   });
