d9946909b7fc7f55ad3c2fedcc0fa78ffa0e91ea
Gakegakwe <87799316+Gakegakwe@users.noreply.github.com>
2021-10-06 10:05:19 +0200

    Use _id to search for user

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -8,7 +8,7 @@ const { getPlatformQuiz } = require("../utils/quizUtils");
 
 exports.getAllQs = async (req, res) => {
   try {
-    let quiz = await Quiz.find({ title: req.body.title }).exec();
+    let quiz = await Quiz.find({_id: req.body._id}).exec();
 
     if (quiz.length === 0) {
       return res.send("Quiz not found");
