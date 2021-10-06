6724514eba1b270c9b2b2a15544ba73849ec4a48
Gakegakwe <87799316+Gakegakwe@users.noreply.github.com>
2021-10-06 09:50:48 +0200

    Search for the Quiz using Id

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -9,7 +9,7 @@ const { getPlatformQuiz } = require("../utils/quizUtils");
 
 exports.getOneQuiz = async (req, res) => {
   try {
-    let quiz = await Quiz.find({ title: req.body.title }).lean(true).exec();
+    let quiz = await Quiz.find({_id: req.body._id }).lean(true).exec();
     if (quiz.length === 0) {
       return res.send("Quiz not found");
     } else {
