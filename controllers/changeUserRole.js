9584084e37f5c348300a991f4d7ca48dce67ed3e
bulaneNayo <bulane.naeo@bothouniversity.com>
2021-10-07 13:55:42 +0200

    added condition for only admins to change roles

--- controllers/changeUserRole.js
+++ controllers/changeUserRole.js
@@ -3,11 +3,14 @@ const { Role } = require("../utils/Constants");
 exports.ChangeUserRole = async (res, req) => {
   try {
     Role.PLAYER === player;
-    if (Role.PLAYER) {
+
+    if (!Role.PLAYER) {  
       const userRole = users
         .findByIdAndUpdate(_id, { $set: { role: Role.ADMIN } })
         .exec();
       return res.status(200).json(userRole);
+    }else{
+        return res.send("You dont have permissions to change roles");
     }
   } catch (error) {
     console.log(error);
