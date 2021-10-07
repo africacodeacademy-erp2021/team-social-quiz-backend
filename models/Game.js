67ed7af8ff71e4a3841144e5d2c81e3f0c7f64c0
bulaneNayo <bulane.naeo@bothouniversity.com>
2021-10-07 13:00:05 +0200

    added game model

---
+++ models/Game.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const GameSchema = new mongoose.Schema(
+  {
+    quiz_id: {
+      type: ObjectId,
+      ref: "Quiz",
+    },
+
+    players: {
+      type: Array,
+    },
+
+    player_answers: {
+      type: Array,
+    },
+
+    winner: {
+      type: ObjectId,
+      ref: "User",
+    },
+
+    winning_score: {
+      type: Number,
+      default: 0,
+    },
+
+    winning_team: {
+      type: Object,
+    },
+
+    leaderboard: {
+      type: ObjectId,
+      ref: "Leaderboard",
+    },
+
+    teams: {
+      type: Array,
+    },
+
+    start_time: {
+      type: Date,
+    },
+
+    end_time: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Game = mongoose.model("Game", GameSchema);
+
+module.exports = Game;
