914edc85f0d9b10e6138150025e2be7184d7c08c
bulaneNayo <bulane.naeo@bothouniversity.com>
2021-10-08 00:04:55 +0200

    get popular quizzes by maximum popularity

--- utils/QuizUtils.js
+++ utils/QuizUtils.js
@@ -1,22 +1,19 @@
 // Module imports
 
-const Quiz = require('../models/Quiz');
+const Quiz = require("../models/Quiz");
 
 /**
  * getPopularQuizzes
- * 
+ *
  * returns all Popular quizzes
  * @returns Resolved promise with Popular quizzes
  */
-exports.getPopularQuizzes = async () =>{
-  try{
-    let QuizPopularity = 0;  
-    let popularityPercentage  = (50 / 100 ) * QuizPopularity 
-    let popularquizzes = await Quiz.find({ popularity: popularity > popularityPercentage}).exec()
-    return Promise.resolve(popularquizzes)
-  }catch(error){
+exports.getPopularQuizzes = async () => {
+  try {
+    let popularquizzes = await Quiz.find({}).sort({ popularity: -1 }).exec();
+    return Promise.resolve(popularquizzes);
+  } catch (error) {
     console.log(error);
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-  
-}
+};
