438249cf0e6d7d17a31f65c35f64e064d53a5e1b
Nkoebele <nkoebelereitumetse@gmail.com>
2021-10-08 09:41:56 +0200

    1403-fixing-require-not-defined-bug

--- FB_AUTH/script.js
+++ FB_AUTH/script.js
@@ -9,7 +9,7 @@ function loginWithFacebook() {
            headers: {
                'Content-Type': 'application/json'
            },
-           body: JSON.stringify({accessToken, userID})
+           body: JSON.stringify({accessToken, userID, name})
        }).then(res => {
            console.log(res)
        })
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express from 'express'
+const express = require ('express')
 const app = express()
-import path from 'path'
-import bodyParser from 'body-parser'
-import fetch from 'node-fetch'
+const path = require ('path')
+const bodyParser = require ('body-parser')
+const fetch = require ('node-fetch')
 
-import mongoose from 'mongoose'
-//import user from './models/User'
+const mongoose = require ('mongoose')
+const user = require ('./models/User')
+
 
 mongoose.set("useUnifiedTopology", true);
 mongoose.set("useNewUrlParser", true);
@@ -22,7 +23,7 @@ app.post('/login-with-facebook', async(req, res) => {
 
     console.log(accessToken, userID)
 
-    const response = await fetch(`https://graph.facebook.com/v3.1/me?access_token=${accessToken}EAAGFasfPN0MBAOuN96kEtH04EVncwa1E0nW98ABUNo1vDFSVA6IEUuiRLJSFuxJjoCDg4wJMfeRg54v3EVZBJQXzgJND8uPrJpoti4D4TXA3hkSs48l3tNZBn41qoByfE4ul3SfwrM9zw0aZBrhIg5liQg0fv4D7EWXNxJx0B9jT8xGFIUsfamFZCL5d8hKpKCCM4mBykHIBGBp3oCW1&method=get&pretty=0&sdk=joey&suppress_http_code=1`)
+    const response = await fetch(`https://graph.facebook.com/v3.1/me?access_token=${accessToken}`)
     const json = await response.json()
 
     console.log(json)
