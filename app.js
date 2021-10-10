9ba8409ad8eaca85c7eb51609444f12a6b8f6151
Elias <12845983+eliasbayona@users.noreply.github.com>
2021-10-10 16:51:44 +0200

    endpoint renaming

--- app.js
+++ app.js
@@ -59,8 +59,8 @@ app.get(`${BASE_URL}/catgeories`, categoryController.getCategories)
 
 // Quiz endpoints
 app.post(`${BASE_URL}/quiz`, quizController.createQuiz);
-app.get(`${BASE_URL}/quiz`, quizController.getAllQuiz)
-app.get(`${BASE_URL}/quiz/published`, quizController.getPublishedQuizes)
+app.get(`${BASE_URL}/quizzes`, quizController.getAllQuiz)
+app.get(`${BASE_URL}/quizzes/published`, quizController.getPublishedQuizes)
 app.post(`${BASE_URL}/quiz/publish`, quizController.publishQuiz)
 
 http.createServer(app).listen(process.env.PORT, () => {
