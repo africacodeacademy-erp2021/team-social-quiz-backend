ae8fea94805ea4497171b2fe78b3f6df0414c007
bulaneNayo <bulane.naeo@bothouniversity.com>
2021-10-10 17:55:59 +0200

    changed publish quiz logic

--- controllers/Publish_quiz.js
+++ controllers/Publish_quiz.js
@@ -1,19 +1,16 @@
-const { Quiz } = require("../models/Quiz");
-const express = require("express");
-const router = express.Router();
+
+const  Quiz  = require("../models/Quiz");
 
 /**
  * find Quiz by id, set its published status to true and return the updated quiz.
  */
 
 exports.QuizPublish = async (req, res) => {
-  
     try {
-      let is_published = false;
-      if (is_published) {
-        const QuizToPublish = await Quiz.findByIdAndUpdate(
-          _id,
-          { $set: { is_published: true } },
+
+        const QuizToPublish = await Quiz.findOneAndUpdate(
+          { _id: req.body._id},
+           { is_published: true } ,
 
           {
             title: req.body.title,
@@ -25,13 +22,8 @@ exports.QuizPublish = async (req, res) => {
             total_score: req.body.total_score,
           }
         ).exec();
-
-        await QuizToPublish.save();
-
         return res.status(200).json(QuizToPublish);
-      } else {
-        return res.status(404).json();
-      }
+      
     } catch (error) {
       console.log(error);
       return res.status(500).json({ error: error });
