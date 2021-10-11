40cee308d219bfc966b901b5fa8d989bebd74630
bulaneNayo <bulane.naeo@bothouniversity.com>
2021-10-11 08:59:09 +0200

    changed remove quiz logic

--- controllers/remove_quiz.js
+++ controllers/remove_quiz.js
@@ -1,32 +1,28 @@
-const { Quiz } = require("../models/Quiz");
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URI);
-const express = require("express");
-const router = express.Router();
 
-/**
- * find and remove Quiz from public view
- */
+const  Quiz  = require("../models/Quiz");
 
- exports.QuizDelete = async () => {
-    router.delete("/Quiz/:_id", async (req, res) => {
-      try {
-        
-        if (is_published === true) {
-          const deleteQuestion = await Quiz
-            .findOneAndRemove({ _id })
-            .exec()
-            .then((Quiz) => res.send(`${Quiz.title}removed from public view`));
-  
-          if ((deleteQuestion.deleteCount = 0)) {
-            return res.status(404).json();
-          } else {
-            return res.status(204).json();
+exports.RemoveQuiz = async (req, res) => {
+    try {
+
+        const QuizToRemove = await Quiz.findOneAndUpdate(
+          {_id: req.body._id},
+           { is_published: false } ,
+
+          {
+            title: req.body.title,
+            description: req.body.description,
+            question_id: req.body.question_id,
+            category_id: req.body.category_id,
+            games: req.body.games,
+            is_published: req.body.is_published,
+            total_score: req.body.total_score,
           }
-        }
-      } catch (error) {
-        return res.status(500).json({ error: error });
-      }
-    });
-  };
+        ).exec();
+        return res.status(200).json(QuizToRemove);
+      
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: error });
+    }
   
+};
