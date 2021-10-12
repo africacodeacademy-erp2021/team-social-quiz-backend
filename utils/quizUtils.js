ab456d89f0ee1685fc1bb25ace8312c3447138a6
Gakegakwe <87799316+Gakegakwe@users.noreply.github.com>
2021-10-12 16:56:30 +0200

    1.Imported User Model and added var UserHistory

--- utils/quizUtils.js
+++ utils/quizUtils.js
@@ -1,21 +1,18 @@
-const { ObjectId } = require('mongodb');
-const Quiz = require('../models/Quiz');
+const { ObjectId } = require("mongodb");
+const User = require("../models/User");
 
 /**
  * getAllGames
- * 
+ *
  * returns all platform Quizes
  * @returns Resolved promise with Platform Quizes
- * @param {String} email 
+ * @param {String} email
  */
-exports.getAllGamesByPlayer = async () =>{
-  try{
-    let quiz = await User.find({email},
-      { game_history: 1 })
-      .exec()
-    return Promise.resolve(quiz)
-  }catch(error){
-    return Promise.reject(error)
+exports.getAllGamesByPlayer = async () => {
+  try {
+    let UserHistory = await User.find({ email:req.body.email }, { game_history: 1 }).exec();
+    return Promise.resolve(UserHistory);
+  } catch (error) {
+    return Promise.reject(error);
   }
-  
-}
+};
