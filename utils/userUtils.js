487747588e5019d216ed3fe99ee9e53171c6c367
bulaneNayo <bulane.naeo@bothouniversity.com>
2021-10-12 17:12:40 +0200

    added getPlayerInfo logic

--- utils/userUtils.js
+++ utils/userUtils.js
@@ -1,19 +1,40 @@
 // Module imports
-
-const User = require('../models/User');
+const { ObjectId } = require('mongodb');
+const User = require("../models/User");
 
 /**
  * getPlatformUsers
- * 
+ *
  * returns all platform users
  * @returns Resolved promise with Platform users
  */
-exports.getPlatformUsers = async () =>{
-  try{
-    let users = await User.find({}).exec()
-    return Promise.resolve(users)
-  }catch(error){
-    return Promise.reject(error)
+exports.getPlatformUsers = async () => {
+  try {
+    let users = await User.find({}).exec
+    return Promise.resolve(users);
+  } catch (error) {
+    console.log(error)
+    return Promise.reject(error);
   }
-  
-}
+};
+/**
+ * getPlayerById
+ * 
+ * returns a user with given playerId
+ * 
+ * @param {String} Id 
+ * @returns Resolved promise with user info
+ */
+
+exports.getPlayerInfo = async (Id) => {
+  try {
+    
+    let user = await User.findOne({ _id:new  ObjectId(Id)}).exec();
+    return Promise.resolve(user)
+    
+  } catch (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
+};
+
