240baedaef6d6d81e3164746adae85503baf239e
Gakegakwe <87799316+Gakegakwe@users.noreply.github.com>
2021-10-13 10:16:18 +0200

    User Controller,not Quiz Controller(Moved functions to the appropriate files)

--- controllers/user.js
+++ controllers/user.js
@@ -2,38 +2,22 @@
 
 const User = require('../models/User');
 
-const { getPlatformUsers } = require("../utils/userUtils")
+const { getAllGamesByPlayer } = require("../utils/userUtils")
 
-exports.getAllUsers = async (req, res) =>{
-    try{
 
-        let usersList = await getPlatformUsers()
-        
-        if(usersList.length > 0 ){
-             return res.send(usersList)
-        }else{
-            return res.status(204).send(usersList)
-        }
-        
-
-    }catch(error){
-        return res.sendStatus(500)
-    }
-    
-}
 exports.getAllGames = async (req, res) => {
-    try {
-      let playerHistory = await User.find({_id: req.body._id},
-      { game_history: 1 }
-    ).exec();;
-  
-      if (playerHistory === 0) {
-        return res.send("User doesn't exist");
-      } else {
-       return res.send(playerHistory); 
-      }
-    } catch (error) {
-      console.log(error)
-      return res.sendStatus(500);
+  try {
+    var { email } = req.body;
+
+    let playerHistory = await getAllGamesByPlayer(email);
+
+    if (playerHistory === 0) {
+      return res.send("User doesn't exist");
+    } else {
+      return res.send(playerHistory);
     }
-  };
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+};
--- utils/userUtils.js
+++ utils/userUtils.js
@@ -13,3 +13,24 @@ exports.getPlatformUsers = async () =>{
   }
   
 }
+const { ObjectId } = require("mongodb");
+const User = require("../models/User");
+
+/**
+ * getAllGames
+ *
+ * returns all platform Quizes
+ * @returns Resolved promise with Platform Quizes
+ * @param {String} email
+ */
+exports.getAllGamesByPlayer = async (email) => {
+  try {
+    let UserHistory = await User.find(
+      { email:email },
+      { game_history: 1 }
+    ).exec();
+    return Promise.resolve(UserHistory);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
