922406c20e159249464a3ebabec22b4551d9ca2f
Gakegakwe <87799316+Gakegakwe@users.noreply.github.com>
2021-10-15 13:09:29 +0200

    added getAllGames route

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ app.get(`${BASE_URL}/questions/quiz`, questionController.getQuizQuestions);
 // User endpoints
 app.get(`${BASE_URL}/users`, userController.getAllUsers);
 app.get(`${BASE_URL}/players`, userController.getPlayers);
+app.get(`${BASE_URL}/player/history`, userController.getAllGames);
 app.get(`${BASE_URL}/admins`, userController.getAdmins);
 app.get(`${BASE_URL}/player`, userController.getPlayer);
 app.put(`${BASE_URL}/player/admin`, userController.elevateToAdmin);
